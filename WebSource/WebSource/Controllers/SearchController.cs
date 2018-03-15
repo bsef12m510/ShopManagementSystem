d89c80d6e8af8f0b3abab158ab4b6ea381c87424
zeeshan <bsef12m510@pucit.edu.pk>
2018-03-16 01:17:53 +0500

    CRUD of shop

--- WebSource/WebSource/Controllers/SearchController.cs
+++ WebSource/WebSource/Controllers/SearchController.cs
@@ -18,8 +18,8 @@ namespace WebSource.Controllers
             //  IEnumerable<product> products = db.products.Where(x => x.product_name.Equals(product_name));
             var products = new List<product>();
             var user_id = userId;
-            var user = db.users.First(y => y.user_id.Equals(user_id));
-            var shop = db.shops.First(y => y.shop_id == user.shop_id);
+            var user = db.users.FirstOrDefault(y => y.user_id.Equals(user_id));
+            var shop = db.shops.FirstOrDefault(y => y.shop_id == user.shop_id);
             var inventory = db.inventories.Where(y => y.shop_id == shop.shop_id);
             
             
@@ -39,9 +39,9 @@ namespace WebSource.Controllers
             var cproducts = new List<CProduct>();
             foreach (var i in inventory)
             {
-                var p = db.products.First(y => y.product_id == i.product_id);
-                var type = db.product_types.First(y => y.type_id == p.product_type);
-                var brand = db.brands.First(y => y.brand_id == p.brand_id);
+                var p = db.products.FirstOrDefault(y => y.product_id == i.product_id);
+                var type = db.product_types.FirstOrDefault(y => y.type_id == p.product_type);
+                var brand = db.brands.FirstOrDefault(y => y.brand_id == p.brand_id);
                 if (type.type_name.Equals(product)) {     
                     cproducts.Add(new CProduct(p,type,brand,i.prod_quant));
                 }
@@ -55,16 +55,16 @@ namespace WebSource.Controllers
         public IHttpActionResult SearchBrand(string userId, string brandName)
         {
             SMS_DBEntities1 db = new SMS_DBEntities1();
-            var user = db.users.First(y => y.user_id.Equals(userId));
-            var shop = db.shops.First(y => y.shop_id == user.shop_id);
+            var user = db.users.FirstOrDefault(y => y.user_id.Equals(userId));
+            var shop = db.shops.FirstOrDefault(y => y.shop_id == user.shop_id);
             var inventory = db.inventories.Where(y => y.shop_id == shop.shop_id);
 
             var cproducts = new List<CProduct>();
             foreach (var i in inventory)
             {
-                var p = db.products.First(y => y.product_id == i.product_id);
-                var type = db.product_types.First(y => y.type_id == p.product_type);
-                var brand = db.brands.First(y => y.brand_id == p.brand_id);
+                var p = db.products.FirstOrDefault(y => y.product_id == i.product_id);
+                var type = db.product_types.FirstOrDefault(y => y.type_id == p.product_type);
+                var brand = db.brands.FirstOrDefault(y => y.brand_id == p.brand_id);
                 if (brand.brand_name.Equals(brandName))
                 {
                     cproducts.Add(new CProduct(p, type, brand, i.prod_quant));
---
+++ WebSource/WebSource/Controllers/ShopController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebSource.Models;
+
+namespace WebSource.Controllers
+{
+    public class ShopController : ApiController
+    {
+        [HttpGet]
+        [ActionName("GetShop")]
+        public IHttpActionResult GetShop(String apiKey , String shopName)
+        {
+            SMS_DBEntities1 db = new SMS_DBEntities1();
+            var user = db.users.First(x => x.api_key.Equals(apiKey));
+            if (user.role_id.Equals("admin"))
+            {
+                var shop = db.shops.FirstOrDefault(x => x.shope_name.Equals(shopName));
+              
+                return Ok(new CShop(shop));
+            }
+            else
+            {
+                return BadRequest();
+            }
+            
+        }
+
+
+        [HttpGet]
+        [ActionName("GetAllShops")]
+        public IHttpActionResult GetAllShops(String apiKey)
+        {
+            SMS_DBEntities1 db = new SMS_DBEntities1();
+            var user = db.users.First(x => x.api_key.Equals(apiKey));
+            if (user.role_id.Equals("admin"))
+            {
+                var shops = db.shops.ToList();
+                var cshops = new List<CShop>();
+                foreach (shop s in shops)
+                    cshops.Add(new CShop(s));
+                return Ok(cshops);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPost]
+        [ActionName("CreateShop")]
+        public IHttpActionResult Post(String apiKey, [FromBody]shop shop)
+        {
+            SMS_DBEntities1 db = new SMS_DBEntities1();
+            var user = db.users.First(x => x.api_key.Equals(apiKey));
+            if (user.role_id.Equals("admin"))
+            {
+                db.shops.Add(shop);
+                db.SaveChanges();
+                return Ok(new CShop(shop));
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut]
+        [ActionName("UpdateShop")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpDelete]
+        [ActionName("DeleteShop")]
+        public IHttpActionResult Delete(String apiKey, int id)
+        {
+            SMS_DBEntities1 db = new SMS_DBEntities1();
+            var user = db.users.First(x => x.api_key.Equals(apiKey));
+            if (user.role_id.Equals("admin"))
+            {
+                var shops = db.shops.ToList();
+
+                List<string> usernames = new List<string>();
+                foreach (var shop in shops)
+                {
+                    if (shop.shop_id == id)
+                    {
+                        shops.Remove(shop);
+                        db.Entry(shop).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        return Ok(new CShop(shop));
+                    }
+
+                }
+                return BadRequest();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
+
+   
+
+
