6832848746328db170a8c882b312049fc1f84558
Hassan Abdur Rauf <hassanabdurrauf@hotmail.com>
2018-04-19 23:51:09 +0500

    Purchase Updated
    Purchase Flow updated as required
    ~ Editing Product Type, Brand
    ~ Invoice ID + Purchase Date time user given handled

--- WebSource/WebSource/Models/JPurchase.cs
+++ WebSource/WebSource/Models/JPurchase.cs
@@ -7,11 +7,13 @@ namespace WebSource.Models
 {
     public class JPurchase
     {
+        public int purch_id { get; set; }
         public JProduct[] products { get; set; }
         public String dlr_name { get; set; }
         public String dlr_phone { get; set; }
         public String apiKey { get; set; }
         public double amount_paid { get; set; }
+        public DateTime purch_dtime { get; set; }
         public double total_amount { get; set; }
         public double discount { get; set; }
     }
