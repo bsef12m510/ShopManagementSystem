007bd442fb833e439cbef6244c1f2991fbe7a4ca
Hassan Abdur Rauf <hassanabdurrauf@hotmail.com>
2018-04-25 01:44:02 +0500

    Purchase updated
    Purchase updated

--- WebSource/WebSource/Controllers/PurchaseController.cs
+++ WebSource/WebSource/Controllers/PurchaseController.cs
@@ -12,7 +12,7 @@ namespace WebSource.Controllers
         [ActionName("PurchaseProduct")]
         public IHttpActionResult purchaseProducts(JPurchase purchase)
         {
-           
+
             try
             {
                 SMS_DBEntities1 db = new SMS_DBEntities1();
@@ -27,8 +27,9 @@ namespace WebSource.Controllers
 
                 List<purchase> purchases = new List<purchase>();
 
-                try {
-                    if(null != db.purchases.FirstOrDefault(y => y.purch_id == purchase.purch_id))
+                try
+                {
+                    if (null != db.purchases.FirstOrDefault(y => y.purch_id == purchase.purch_id))
                     {
                         return Ok(-1);
                     }
@@ -38,76 +39,82 @@ namespace WebSource.Controllers
                 foreach (var product in purchase.products)
                 {
                     var prod_id = product.product_id;
-                    
-                    if (prod_id == 0)
+
+
+                    if (null == db.brands.FirstOrDefault(y => y.brand_id == product.brand.brand_id) &&
+                        null == db.brands.FirstOrDefault(y => y.brand_name == product.brand.brand_name))
                     {
-                        if (null == db.brands.FirstOrDefault(y => y.brand_id == product.brand.brand_id) &&
-                            null == db.brands.FirstOrDefault(y => y.brand_name == product.brand.brand_name))
-                        {
-                            db.brands.Add(new brand { brand_name = product.brand.brand_name });
-                            db.SaveChanges();
-                        }
-                        else if(null != db.brands.FirstOrDefault(y => y.brand_id == product.brand.brand_id) &&
-                                product.brand.brand_name != null && !product.brand.brand_name.Equals(""))
-                        {
-                            brand b = db.brands.FirstOrDefault(y => y.brand_id == product.brand.brand_id);
-                            b.brand_name = product.brand.brand_name;
-                            db.SaveChanges();
-                        }
-                        if (null == db.product_types.FirstOrDefault(y => y.type_id == product.product_type.type_id) &&
-                            null == db.product_types.FirstOrDefault(y => y.type_name == product.product_type.type_name))
-                        {
-                            db.product_types.Add(new product_types { type_name = product.product_type.type_name });
-                            db.SaveChanges();
-                        }
-                        else if (null != db.product_types.FirstOrDefault(y => y.type_id == product.product_type.type_id) &&
-                                product.product_type.type_name != null && !product.product_type.type_name.Equals(""))
-                        {
-                            product_types p = db.product_types.FirstOrDefault(y => y.type_id == product.product_type.type_id);
-                            p.type_name = product.product_type.type_name;
-                            db.SaveChanges();
-                        }
-                        if (null == db.msrmnt_units.FirstOrDefault(y => y.sr_no.Equals(product.unit_of_msrmnt.sr_no))){
-                            db.msrmnt_units.Add(new msrmnt_units { description = product.unit_of_msrmnt.description });
-                            db.SaveChanges();
-                        }
-                        else 
-                        {
-                            var msmnt_unit = db.msrmnt_units.FirstOrDefault(y => y.sr_no.Equals(product.unit_of_msrmnt.sr_no));
-                            msmnt_unit.description = product.unit_of_msrmnt.description;
-                            db.SaveChanges();
-                        }
-                        if (null == db.products.FirstOrDefault(y => y.product_id == product.product_id))
-                        {
-                            var brand_id = product.brand.brand_id;
-                            var product_type = product.product_type.type_id;
-
-                            if (product_type == 0)
-                                product_type = db.product_types.FirstOrDefault(y => y.type_name == product.product_type.type_name).type_id;
-                            if (brand_id == 0)
-                                brand_id = db.brands.FirstOrDefault(y => y.brand_name == product.brand.brand_name).brand_id;
-
-                            db.products.Add(new product
-                            {
-                                product_name = product.product_name,
-                                unit_price = product.unit_price,
-                                unit_of_msrmnt = product.unit_of_msrmnt.description,
-                                specs = product.specs,
-                                brand_id = brand_id,
-                                product_type = product_type
-                            });
-
-                            db.SaveChanges();
-                        }
-                        prod_id = db.products.First(y => y.product_name.Equals(product.product_name)).product_id;
+                        db.brands.Add(new brand { brand_name = product.brand.brand_name });
+                        db.SaveChanges();
                     }
-                    
+                    else if (null != db.brands.FirstOrDefault(y => y.brand_id == product.brand.brand_id) &&
+                            product.brand.brand_name != null && !product.brand.brand_name.Equals(""))
+                    {
+                        brand b = db.brands.FirstOrDefault(y => y.brand_id == product.brand.brand_id);
+                        b.brand_name = product.brand.brand_name;
+                        db.SaveChanges();
+                    }
+                    if (null == db.product_types.FirstOrDefault(y => y.type_id == product.product_type.type_id) &&
+                        null == db.product_types.FirstOrDefault(y => y.type_name == product.product_type.type_name))
+                    {
+                        db.product_types.Add(new product_types { type_name = product.product_type.type_name });
+                        db.SaveChanges();
+                    }
+                    else if (null != db.product_types.FirstOrDefault(y => y.type_id == product.product_type.type_id) &&
+                            product.product_type.type_name != null && !product.product_type.type_name.Equals(""))
+                    {
+                        product_types p = db.product_types.FirstOrDefault(y => y.type_id == product.product_type.type_id);
+                        p.type_name = product.product_type.type_name;
+                        db.SaveChanges();
+                    }
+                    if (null == db.msrmnt_units.FirstOrDefault(y => y.sr_no.Equals(product.unit_of_msrmnt.sr_no)))
+                    {
+                        db.msrmnt_units.Add(new msrmnt_units { description = product.unit_of_msrmnt.description });
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        var msmnt_unit = db.msrmnt_units.FirstOrDefault(y => y.sr_no.Equals(product.unit_of_msrmnt.sr_no));
+                        msmnt_unit.description = product.unit_of_msrmnt.description;
+                        db.SaveChanges();
+                    }
+                    if (null == db.products.FirstOrDefault(y => y.product_id == product.product_id))
+                    {
+                        var brand_id = product.brand.brand_id;
+                        var product_type = product.product_type.type_id;
+
+                        if (product_type == 0)
+                            product_type = db.product_types.FirstOrDefault(y => y.type_name == product.product_type.type_name).type_id;
+                        if (brand_id == 0)
+                            brand_id = db.brands.FirstOrDefault(y => y.brand_name == product.brand.brand_name).brand_id;
+
+                        db.products.Add(new product
+                        {
+                            product_name = product.product_name,
+                            unit_price = product.unit_price,
+                            unit_of_msrmnt = product.unit_of_msrmnt.description,
+                            specs = product.specs,
+                            brand_id = brand_id,
+                            product_type = product_type
+                        });
+
+                        db.SaveChanges();
+                    }
+                    prod_id = db.products.First(y => y.product_name.Equals(product.product_name)).product_id;
+
+                    if (0 != product.product_id) {
+                        var prod = db.products.First(y => y.product_name.Equals(product.product_name));
+                        prod.product_name = product.product_name;
+                        db.SaveChanges();
+                    }
+
                     var invObj = inventory.FirstOrDefault(y => y.product_id == product.product_id);
                     if (invObj != null)
                     {
                         invObj.prod_quant += product.qty;
                     }
-                    else {
+                    else
+                    {
                         db.inventories.Add(new inventory
                         {
                             product_id = prod_id,
@@ -133,13 +140,14 @@ namespace WebSource.Controllers
                             shop_id = shop.shop_id,
                             agent_id = user.user_id,
                             is_pmnt_clr = isClr,
-                            total_amt = purchase.total_amount,                         
+                            total_amt = purchase.total_amount,
                             paid_amt = purchase.amount_paid,
                             is_invoice = "Y"
 
                         });
                     }
-                    else {
+                    else
+                    {
                         purchases.Add(new purchase
                         {
                             purch_id = purchase.purch_id,
@@ -158,7 +166,8 @@ namespace WebSource.Controllers
                     i++;
                 }
 
-                foreach (var purchase1 in purchases) {
+                foreach (var purchase1 in purchases)
+                {
                     db.purchases.Add(purchase1);
                 }
 
