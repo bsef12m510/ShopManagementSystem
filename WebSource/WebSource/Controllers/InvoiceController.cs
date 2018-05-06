19912f28cfbea8aa8c73c9139bd28323d5ecf8b7
Hassan Abdur Rauf <hassanabdurrauf@hotmail.com>
2018-05-06 15:51:24 +0500

    Invoice updated
    Invoice updated

--- WebSource/WebSource/Controllers/InvoiceController.cs
+++ WebSource/WebSource/Controllers/InvoiceController.cs
@@ -11,8 +11,8 @@ namespace WebSource.Controllers
     public class InvoiceController : ApiController
     {
 
-        [ActionName("GetInvoice")]
-        public IHttpActionResult getInvoice(String apiKey, int invoiceId)
+        [ActionName("GetSalesInvoice")]
+        public IHttpActionResult getSalesInvoice(String apiKey, int invoiceId)
         {
             JInvoice invoice = null;
 
@@ -27,8 +27,36 @@ namespace WebSource.Controllers
                 var shop = db.shops.FirstOrDefault(y => y.shop_id == user.shop_id);
 
                 invoice = new JInvoice(db.sales.FirstOrDefault(y => y.sale_id == invoiceId));
-              //  if (null == invoice)
-                  //  invoice = new JInvoice(db.purchases.FirstOrDefault(y => y.purch_id == str_invoiceId));
+            }
+            catch (Exception ex)
+            {
+                //ok = false;
+            }
+            finally { }
+
+            return Ok(invoice);
+        }
+
+        [ActionName("GetPurchaseInvoice")]
+        public IHttpActionResult GetPurchaseInvoice(String apiKey, String invoiceId)
+        {
+            JInvoice invoice = null;
+
+            try
+            {
+                SMS_DBEntities1 db = new SMS_DBEntities1();
+                var user = db.users.FirstOrDefault(y => y.api_key.Equals(apiKey));
+                if (null == user)
+                {
+                    return Ok();
+                }
+                var shop = db.shops.FirstOrDefault(y => y.shop_id == user.shop_id);
+
+                invoice = new JInvoice(db.purchases.FirstOrDefault(y => y.purch_id == str_invoiceId
+                                                                       && y.is_invoice = "Y" && y.is_pmnt_clr = "N"));
+
+                if (null == invoice)
+                    return OK(fals);
 
             }
             catch (Exception ex)
@@ -40,49 +68,38 @@ namespace WebSource.Controllers
             return Ok(invoice);
         }
 
-        [ActionName("GetAllInvoices")]
-        public IHttpActionResult getAllInvoices(JInvoice invoice)
+        [ActionName("clearSaleInvoicePayment")]
+        public IHttpActionResult clearSaleInvoicePayment(String apiKey, String invoiceId)
         {
-            //bool ok = true;
-            //try
-            //{
-            //    SMS_DBEntities1 db = new SMS_DBEntities1();
-            //    var user = db.users.FirstOrDefault(y => y.api_key.Equals(invoice.apiKey));
-            //    if (null == user)
-            //    {
-            //        return Ok();
-            //    }
-            //    var shop = db.shops.FirstOrDefault(y => y.shop_id == user.shop_id);
+            JInvoice invoice = null;
 
-            //    sale sInvoice = db.sales.FirstOrDefault(y => y.sale_id == invoice.invoiceId && y.is_invoice.equals("Y"));
-            //    if (sInvoice != null)
-            //    {
-            //        if (invoice.amount_paid == sInvoice.total_amt)
-            //            sInvoice.is_pmnt_clr = "Y";
-            //        sInvoice.paid_amt = invoice.amount_paid;
-            //    }
-            //    else
-            //    {
-            //        purchase pInvoice = db.purchases.FirstOrDefault(y => y.purch_id == invoice.invoiceId && y.is_invoice.equals("Y"));
-            //        if (sInvoice != null)
-            //        {
-            //            if (invoice.amount_paid == pInvoice.total_amt)
-            //                sInvoice.is_pmnt_clr = "Y";
-            //            pInvoice.paid_amt = invoice.amount_paid;
-            //        }
-            //        else
-            //            ok = false;
-            //    }
-                
-            //    db.SaveChanges();
-            //}
-            //catch (Exception ex)
-            //{
-            //    ok = false;
-            //}
-            //finally { }
+            try
+            {
+                SMS_DBEntities1 db = new SMS_DBEntities1();
+                var user = db.users.FirstOrDefault(y => y.api_key.Equals(apiKey));
+                if (null == user)
+                {
+                    return Ok();
+                }
+                var shop = db.shops.FirstOrDefault(y => y.shop_id == user.shop_id);
 
-            return Ok();
+                invoice = new JInvoice(db.purchases.FirstOrDefault(y => y.purch_id == str_invoiceId
+                                                                       && y.is_invoice = "Y" && y.is_pmnt_clr = "N"));
+
+                if (null == invoice)
+                    return false;
+
+                invoice.total_amt = invoice.paid_amt;
+                invoice.is_pmnt_clr = "Y";
+
+            }
+            catch (Exception ex)
+            {
+                //ok = false;
+            }
+            finally { }
+
+            return Ok(true);
         }
     }
 }
