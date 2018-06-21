81c2099e902e27af1b1401d30de6261fd7935a34
Zeeshan Awan <bsef12m510@pucit.edu.pk>
2018-06-22 03:23:09 +0500

    changes

--- androidApp/POS/app/src/main/java/com/apptriangle/pos/sales/fragment/InvoiceFragment.java
+++ androidApp/POS/app/src/main/java/com/apptriangle/pos/sales/fragment/InvoiceFragment.java
@@ -25,6 +25,7 @@ import android.widget.Toast;
 import com.apptriangle.pos.InvoiceSearchFragment.adapter.InvoiceAdapter;
 import com.apptriangle.pos.InvoiceSearchFragment.fragment.InvoiceSearchFragment;
 import com.apptriangle.pos.InvoiceSearchFragment.service.InvoiceService;
+import com.apptriangle.pos.MainDrawerActivity;
 import com.apptriangle.pos.R;
 import com.apptriangle.pos.SecureActivity;
 import com.apptriangle.pos.api.ApiClient;
@@ -93,6 +94,7 @@ public class InvoiceFragment extends Fragment {
         public void onTextChanged(CharSequence s, int start, int before, int count) {
         }
     };
+    private String role;
 
     public InvoiceFragment() {
         // Required empty public constructor
@@ -102,7 +104,13 @@ public class InvoiceFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setTitle();
+        getSavedHeaderData();
         initialize();
+    }
+
+    private void getSavedHeaderData() {
+        SharedPreferences shared = getActivity().getSharedPreferences("com.appTriangle.pos", Context.MODE_PRIVATE);
+        role = shared.getString("role", "");
     }
 
     @Override
@@ -200,14 +208,14 @@ public class InvoiceFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if (cart != null) {
-                    Intent intent = new Intent(getActivity(), SecureActivity.class);
+                    Intent intent = new Intent(getActivity(), "owner".equalsIgnoreCase(role) ? MainDrawerActivity.class : SecureActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
                 } else {
                     if (edtPaidAmnt.isEnabled())
                         updateInvoice();
                     else {
-                        Intent intent = new Intent(getActivity(), SecureActivity.class);
+                        Intent intent = new Intent(getActivity(), "owner".equalsIgnoreCase(role) ? MainDrawerActivity.class : SecureActivity.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent);
                     }
@@ -215,7 +223,6 @@ public class InvoiceFragment extends Fragment {
             }
         });
     }
-
 
     public void updateInvoice() {
         boolean isOk = false;
